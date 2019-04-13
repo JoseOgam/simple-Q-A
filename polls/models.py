d5ba50418caebf4227759c01cb332bb55f49c83d
Jose <jotienoochieng@gmail.com>
2019-04-13 11:36:16 +0300

    third commit

--- polls/models.py
+++ polls/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 
 from django.db import models
+from django.utils import timezone
+from django.utils.datetime_safe import datetime
 
 
 class Question(models.Model):
@@ -14,3 +16,7 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
--- polls/views.py
+++ polls/views.py
@@ -12,6 +12,8 @@ from django.template import loader
 
 from django.urls import reverse
 
+from django.utils import timezone
+
 from .models import Choice, Question
 
 
@@ -79,3 +81,29 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+    class IndexView(generic.ListView):
+        template_name = 'polls/index.html'
+        context_object_name = 'latest_question_list'
+
+        def get_queryset(self):
+            """Return the last five published questions."""
+            return Question.objects.order_by('-pub_date')[:5]
+
+        def get_queryset(self):
+            """
+            Return the last five published questions (not including those set to be
+            published in the future).
+            """
+            return Question.objects.filter(
+                pub_date__lte=timezone.now()
+            ).order_by('-pub_date')[:5]
+
+        class DetailView(generic.DetailView):
+            ...
+
+            def get_queryset(self):
+                """
+                Excludes any questions that aren't published yet.
+                """
+                return Question.objects.filter(pub_date__lte=timezone.now())
